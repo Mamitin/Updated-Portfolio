8920f3150c2d45741f0b3c825f99ac3d4cb347c2
Amitin <MAmitin@academyart.edu>
2019-11-18 14:47:40 -0800

    finished adding images, links and cards to portfolio section

---
+++ assets/javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var navBarHeight = $(".navbar").outerHeight();
+    $(".nav-link").click(function(){
+        var linkHref = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top - navBarHeight
+        }, 3000);
+    })
+
+
+}
+
+
+)
